f1fcb38cf93ad598ccea3ceac1cefb62b1b65460
DenHax <whatxpw@gmail.com>
2024-08-09 18:24:35 +0300

    Initial commit for LibreLib backend: add .gitignore, main.go

---
+++ README.md
@@ -0,0 +1,5 @@
+Backend for LibreLib.
+API:
+
+Stack:
+Golang 1.21.9
---
+++ devenv.nix
@@ -0,0 +1,50 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  env.GREET = "devenv";
+
+  packages = with pkgs; [
+    git
+    docker
+    docker-compose
+  ];
+
+  # Main service language
+  languages.go = {
+    enable = true;
+    package = pkgs.go_1_21;
+  };
+
+  # Main database
+  # services.postgres = {
+  #   enable = true;
+  #   package = pkgs.postgresql_15;
+  #   initialDatabases = [ { name = "LibreLib"; } ];
+  # };
+
+  # Web-proxy
+  # services.nginx.enable = true;
+
+  # Cache for service
+  # services.redis.enable = true;
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    Backend for LibreLib
+    go version
+  '';
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+}
