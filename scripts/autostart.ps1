0cf956d586a71062868145d4e26b9ae09c1206cc
DenHax <whatxpw@gmail.com>
2024-09-07 18:01:50 +0300

    Refactored autostart scripts: add checking on existence

--- scripts/autostart.ps1
+++ scripts/autostart.ps1
@@ -1,14 +1,28 @@
-#!/usr/bin/env pwsh
+# Проверка на существование файла .env
+if (Test-Path -Path ".\.env") {
+    Write-Host "Error: .env file already exists"
+} else {
+    # Создание .env файла
+    @"
+POSTGRES_PASSWORD=p4ssw0rd
+POSTGRES_PORT=5432
+POSTGRES_HOST=postgres
+DB_USER=librelib-admin
+DB_NAME=librelib
+DATABASE_URL=postgres://\$DB_USER:\$POSTGRES_PASSWORD@\$POSTGRES_HOST:\$POSTGRES_PORT/\$DB_NAME?sslmode=disable
+"@ | Set-Content -Path ".\.env"
+    Write-Host ".env file created successfully"
+}
 
-@"
-$env:POSTGRES_PASSWORD = "p4ssw0rd"
-$env:POSTGRES_PORT = "5432"
-$env:POSTGRES_HOST = "postgres"
-$env:DB_USER = "librelib-admin"
-$env:DB_NAME = "librelib"
-$env:DATABASE_URL = "postgres://$($env:DB_USER):$($env:POSTGRES_PASSWORD)@$($env:POSTGRES_HOST):$($env:POSTGRES_PORT)/$($env:DB_NAME)?sslmode=disable"
-"@ | Set-Content -Path ..\.env.ps1
-
-. ..\.env.ps1
-
-Write-Host "Environment activation: succeeded"
+# Загрузка переменных окружения
+if (Test-Path -Path ".\.env") {
+    Get-Content -Path ".\.env" | ForEach-Object {
+        if ($_ -and $_ -notmatch '^#') {
+            $name, $value = $_ -split '=', 2
+            [System.Environment]::SetEnvironmentVariable($name.Trim(), $value.Trim(), [System.EnvironmentVariableTarget]::Process)
+        }
+    }
+    Write-Host "Environment activation: succeeded"
+} else {
+    Write-Host "Error: .env file not found"
+}
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-cat <<EOL >../.env
+if [ -f ./.env ]; then
+  echo "Error: .env file already exists"
+else
+  cat <<EOL >.env
 POSTGRES_PASSWORD=p4ssw0rd
 POSTGRES_PORT=5432
 POSTGRES_HOST=postgres
@@ -8,12 +11,16 @@ DB_USER=librelib-admin
 DB_NAME=librelib
 DATABASE_URL=postgres://\${DB_USER}:\${POSTGRES_PASSWORD}@\${POSTGRES_HOST}:\${POSTGRES_PORT}/\${DB_NAME}?sslmode=disable
 EOL
+  echo ".env file created successfully"
+fi
 
-#!/usr/bin/env bash
-
-if [ -f ../.env ]; then
-  export "$(grep -v '^#' ../.env | xargs)"
-  echo "Environment activation: succeeded"
+if [ -f ./.env ]; then
+  export "$(grep -v '^#' ./.env | xargs)"
+  if [ $? -eq 0 ]; then
+    echo "Environment activation: succeeded"
+  else
+    echo "Error: Failed to export environment variables"
+  fi
 else
   echo "Error: .env file not found"
 fi
