9c73fa9f0745b041286d6b78e96b56e9a96a66db
DenHax <whatxpw@gmail.com>
2024-09-16 09:59:52 +0300

    Add config.go
    Add structs for config in internal/config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,2 +1,51 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Server HTTPserver
+	PSQL   Postgres
+}
+
+type HTTPserver struct {
+	Port int
+}
+
+type Postgres struct {
+	Host     int
+	Port     int
+	User     string
+	Password string
+	Name     string
+	SSLmode  string
+}
+
+type Logger struct {
+	Dev   bool
+	Level string
+}
+
+func MustConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable is not set")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error opening config file: %s", err)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+
+	return &cfg
+}
