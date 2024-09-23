b46a32d29d1505ccd94cd8452713c4c2af4959d0
DenHax <whatxpw@gmail.com>
2024-09-23 12:48:47 +0300

    Refatored rest structure for

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,77 +1,94 @@
 package handlers
 
 import (
-	"database/sql"
 	"encoding/json"
 	"net/http"
-	"strconv"
-
-	"github.com/DenHax/LibreLib-back/internal/database"
+	"sync"
+	// "github.com/DenHax/LibreLib-back/internal/database"
 )
 
-var db *sql.DB = database.GetDB()
+var (
+	mutex sync.Mutex
+)
 
-func GetAllBooks(w http.ResponseWriter, r *http.Request) {
-	bookList, err := database.GetBooks(db)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(bookList)
-}
+func GetNameHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-func GetBooksByCustomerID(w http.ResponseWriter, r *http.Request) {
-	id_str := r.URL.Query().Get("id")
-	id, err := strconv.Atoi(id_str)
-	if err != nil {
-		http.Error(w, "Invalid customer ID", http.StatusBadRequest)
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
 		return
 	}
-	book, err := database.GetBooksByCustomerID(db, id)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(book)
+
+	mutex.Lock()
+	defer mutex.Unlock()
+	json.NewEncoder(w).Encode(map[string]string{"name": "hello"})
 }
 
-func GetBooksByAuthor(w http.ResponseWriter, r *http.Request) {
-	author := r.URL.Query().Get("author")
-	book, err := database.GetBooksByAuthor(db, author)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(book)
-}
+// var db *sql.DB = database.GetDB()
 
-func GetBooksByGenre(w http.ResponseWriter, r *http.Request) {
-	genre := r.URL.Query().Get("genre")
-	book, err := database.GetBooksByGenre(db, genre)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(book)
-}
-
-func GetBooksByYear(w http.ResponseWriter, r *http.Request) {
-	f_year_str := r.URL.Query().Get("f_year")
-	s_year_str := r.URL.Query().Get("s_year")
-	f_year, err := strconv.Atoi(f_year_str)
-	if err != nil {
-		http.Error(w, "Invalid year", http.StatusBadRequest)
-		return
-	}
-	s_year, err := strconv.Atoi(s_year_str)
-	if err != nil {
-		http.Error(w, "Invalid year", http.StatusBadRequest)
-		return
-	}
-	book, err := database.GetBooksByYear(db, f_year, s_year)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(book)
-}
+// func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+// 	bookList, err := database.GetBooks(db)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(bookList)
+// }
+//
+// func GetBooksByCustomerID(w http.ResponseWriter, r *http.Request) {
+// 	id_str := r.URL.Query().Get("id")
+// 	id, err := strconv.Atoi(id_str)
+// 	if err != nil {
+// 		http.Error(w, "Invalid customer ID", http.StatusBadRequest)
+// 		return
+// 	}
+// 	book, err := database.GetBooksByCustomerID(db, id)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(book)
+// }
+//
+// func GetBooksByAuthor(w http.ResponseWriter, r *http.Request) {
+// 	author := r.URL.Query().Get("author")
+// 	book, err := database.GetBooksByAuthor(db, author)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(book)
+// }
+//
+// func GetBooksByGenre(w http.ResponseWriter, r *http.Request) {
+// 	genre := r.URL.Query().Get("genre")
+// 	book, err := database.GetBooksByGenre(db, genre)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(book)
+// }
+//
+// func GetBooksByYear(w http.ResponseWriter, r *http.Request) {
+// 	f_year_str := r.URL.Query().Get("f_year")
+// 	s_year_str := r.URL.Query().Get("s_year")
+// 	f_year, err := strconv.Atoi(f_year_str)
+// 	if err != nil {
+// 		http.Error(w, "Invalid year", http.StatusBadRequest)
+// 		return
+// 	}
+// 	s_year, err := strconv.Atoi(s_year_str)
+// 	if err != nil {
+// 		http.Error(w, "Invalid year", http.StatusBadRequest)
+// 		return
+// 	}
+// 	book, err := database.GetBooksByYear(db, f_year, s_year)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(book)
+// }
--- internal/router/router.go
+++ internal/router/router.go
@@ -8,8 +8,10 @@ import (
 func GetRouter() *mux.Router {
 	router := mux.NewRouter()
 	booksRouter := router.PathPrefix("/books").Subrouter()
-	booksRouter.HandleFunc("", handlers.GetAllBooks).Methods("GET")
-	booksRouter.HandleFunc("/author", handlers.GetBooksByAuthor).Methods("GET")
+	booksRouter.HandleFunc("/author", handlers.GetNameHandler)
+
+	// booksRouter.HandleFunc("", handlers.GetAllBooks).Methods("GET")
+	// booksRouter.HandleFunc("/author", handlers.GetBooksByAuthor).Methods("GET")
 	// router.HandleFunc("/books/genre", handlers.GetBooksByGenre).Methods("GET")
 	// router.HandleFunc("/books/year", handlers.GetBooksByYear).Methods("GET")
 	// router.HandleFunc("/cart", handlers.GetBooksByCustomerID).Methods("GET")
