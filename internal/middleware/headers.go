9e9cdf1bf7994773ea74598d6636464a5fe786c8
DenHax <whatxpw@gmail.com>
2024-10-26 10:49:22 +0300

    Refactoring for middleware name

--- internal/middleware/headers.go
+++ internal/middleware/headers.go
@@ -2,7 +2,7 @@ package middleware
 
 import "net/http"
 
-func Middleware(next http.Handler) http.Handler {
+func HeaderMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Server", "LibreLib")
 		w.Header().Set("Content-Type", "application/json")
