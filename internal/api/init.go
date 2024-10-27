6d0b0eef13858a31d70fe99ba8ef063a791f0b7f
DenHax <whatxpw@gmail.com>
2024-10-27 18:32:39 +0300

    Filler routing refactoring

--- internal/api/init.go
+++ internal/api/init.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/DenHax/LibreLib-back/internal/middleware"
@@ -22,15 +23,25 @@ type Response struct {
 	Name string `json:"name"`
 }
 
-func GetPath(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response := Response{Name: "u name"}
-	json.NewEncoder(w).Encode(response)
+func GetNameHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+	json.NewEncoder(w).Encode(map[string]string{"name": "hello"})
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Main page")
 }
 
 func (h Handler) Init() *mux.Router {
 	r := mux.NewRouter()
-
+	r.HandleFunc("/", helloWorld)
+	r.HandleFunc("/info", GetNameHandler)
 	api := r.PathPrefix("/api").Subrouter()
 	api.Use(middleware.HeaderMiddleware)
 	api = h.NewApi(api)
--- internal/api/router.go
+++ internal/api/router.go
@@ -1,17 +1,12 @@
 package api
 
 import (
-	"github.com/DenHax/LibreLib-back/internal/middleware"
 	"github.com/gorilla/mux"
 )
 
 func (h Handler) NewApi(r *mux.Router) *mux.Router {
-	api := r.PathPrefix("/api/").Subrouter()
-	api.Use(middleware.HeaderMiddleware)
-	v1 := api.PathPrefix("/v1/").Subrouter()
-
-	booksRouter := v1.PathPrefix("/books").Subrouter()
-	// booksRouter.HandleFunc("/", handlers.GetAllBooks(s)).Methods("GET")
+	booksRouter := r.PathPrefix("/books").Subrouter()
+	booksRouter.HandleFunc("/", GetNameHandler).Methods("GET")
 	//
 	// booksRouter.HandleFunc("/author", handlers.GetNameHandler).Methods("GET")
 
@@ -20,5 +15,5 @@ func (h Handler) NewApi(r *mux.Router) *mux.Router {
 	// router.HandleFunc("/books/genre", handlers.GetBooksByGenre).Methods("GET")
 	// router.HandleFunc("/books/year", handlers.GetBooksByYear).Methods("GET")
 	// router.HandleFunc("/cart", handlers.GetBooksByCustomerID).Methods("GET")
-	return api
+	return r
 }
